721c11f9ee2ef64896e01acf5690176da0028220
PrzemysławMorski <morski.przemek@gmail.com>
2019-12-15 11:46:14 +0100

    main.go is now a simple http server; updated wercker.yml to use github.com/PrzemyslawMorski/cloud-native-golang as it's source repo

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-	"github.com/olekukonko/tablewriter"
+	"fmt"
+	"net/http"
 	"os"
+
+	"github.com/codegangsta/negroni"
 )
 
 func main() {
-	data := [][]string{
-		{"Alfred", "15", "10/20", "(10.32, 56.21, 30.25)"},
-		{"Beelzebub", "30", "30/50", "(1,1,1)"},
-		{"Hortense", "21", "80/80", "(1,1,1)"},
-		{"Pokey", "8", "30/40", "(1,1,1)"},
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
 	}
 
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"NPC", "Speed", "Power", "Location"})
-	table.AppendBulk(data)
-	table.Render()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	hostString := fmt.Sprintf(":%s", port)
+	n.Run(hostString)
+}
+
+func hello(res http.ResponseWriter, _ *http.Request) {
+	_, _ = fmt.Fprintln(res, "Hello from Go!")
 }
